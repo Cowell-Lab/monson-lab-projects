b37ede0f187ca2a7b757b66ffdd7bc5a32a3d346
Sam Wollenburg <Samuel.Wollenburg@UTSouthwestern.edu>
2025-05-15 12:45:34 -0500

    update restriction site replacement function

--- ordering_script/ordering.py
+++ ordering_script/ordering.py
@@ -1,6 +1,5 @@
-import sys
 import pandas as pd
-
+import argparse
 
 consts_df = {'vector' : ['IGH', 'IGK', 'IGL'], # 'HC', 'KLC', 'LLC'
              'start' : ['CTGCAACCGGTGTACATTCC', 'TGTGCTGCAACCGGTGTACAT', 'CTGCTACCGGT'],
@@ -33,53 +32,69 @@ codons = {'TTT':'TTC', 'TTC':'TTT', # phe
          }
 
 def restriction_replacement(seq, selector):
-    sites = consts_df['restriction sites'][selector]
+
+    '''
+    Replaces the first codon in a restriction site that is not within the respective start or end regions.
+    The first restriction site should be seen in the start, the second restriction site in the end.
+    
+    Parameters
+    ----------
+    seq : string
+        - The nucleotide sequence to replace restriction sites
+    selector : int
+        - Based on the index of consts_df['vector']. e.g. 'IGH'==0, 'IGK'==1, 'IGL'==2
+    
+    Output
+    ------
+    new_seq : string
+        - The altered nucleotide sequence with replaced codons and no internal restriction sites.
+    '''
+    new_seq = seq
+
+    rsites = consts_df['restriction sites'][selector]
+    ustart = consts_df['start'][selector]
+    uend = consts_df['end'][selector]
     
     # find sites
-    # first restriction site
-    i1 = 0
-    if seq.find(sites[0], seq.find(sites[0])+1) != -1:
-        i1 = seq.find(sites[0], seq.find(sites[0])+1)
-        print(sites[0])
-    else:
-        pass
+    # first restriction site outside of (or overlapping with) the universal start (rs needed there)
+    i1, i2 = 0, 0
+    while i1 != -1 and i2 != -1:
+        # first restriction site outside of (or overlapping with) the universal start (rs needed there)
+        i1 = seq[(len(ustart) - len(rsites[0]) + 1):].find(rsites[0])
+        if i1 != -1:
+            i1 = i1 + len(ustart) - len(rsites[0]) + 1
+            i1 = i1 - (i1 + consts_df['reading frame'][selector]) % 3
+            while i1 < 0:
+                i1 += 3
+            new_seq = seq[0:i1] + codons[seq[i1:i1+3]] + seq[i1+3:]
+            
+        # second restriction site outside of (or overlapping with) the universal end (rs needed there)
+        i2 = seq[:len(new_seq)-len(uend)].find(rsites[1])
+        if i2 != -1:
+            i2 = i2 - (i2 + consts_df['reading frame'][selector]) % 3
+            while i2 < 0:
+                i2 += 3
+            new_seq = seq[0:i2] + codons[seq[i2:i2+3]] + seq[i2+3:]
 
-    # second restriction site
-    i2 = 0
-    if seq.find(sites[1], seq.find(sites[1])+1) != -1:
-        i2 = seq.find(sites[1])
-        print(sites[1])
-    else:
-        pass
-    
-    if i1 == 0 and i2 == 0:
-        return seq
-
-    # replace
-    new_seq = seq
-    for i in range((3-consts_df['reading frame'][selector]), len(seq),3):
-        if i1 >= i and i1 <= i+2:
-            #replace codon
-            new_seq = new_seq[0:i] + codons[new_seq[i:i+3]] + new_seq[i+3:]
-            print(len(seq), len(new_seq))
-            # pass
-        if i2 >= i and i2 <= i+2:
-            #replace codon
-            new_seq = new_seq[0:i] + codons[new_seq[i:i+3]] + new_seq[i+3:]
-            # print(len(seq), len(new_seq))
-            # pass
-        if i1 >=i+2 and i2>=i+2:
-            return new_seq
+    return new_seq
 
 def main():
-    #read data
-    igblast_makedb = pd.read_csv('./data/04072023_YW.igblast.makedb.airr.tsv', delimiter='\t')
-    var_df = pd.read_csv('./data/genes_v_call.csv').drop(columns=['Unnamed: 0'])
+    parser = argparse.ArgumentParser(
+        prog='Ordering Script',
+        description="Produce orderable vectors")
+    parser.add_argument('--data', required=True, help='Path to input igblast makedb airr tsv file; /path/to/<FILE_NAME>.igblast.makedb.aiir.tsv')
+    parser.add_argument('--v_call', required=True, help='Path to v-call reference; /path/to/genes_v_call.csv')
+    args = parser.parse_args()
+
+    # read data
+    # igblast_makedb = pd.read_csv('./data/04072023_YW.igblast.makedb.airr.tsv', delimiter='\t')
+    # var_df = pd.read_csv('./data/genes_v_call.csv').drop(columns=['Unnamed: 0'])
+    igblast_makedb = pd.read_csv(args.data, delimiter='\t')
+    var_df = pd.read_csv(args.v_call).drop(columns=['Unnamed: 0'])
 
     # make order df
     order_df = igblast_makedb.loc[:,['sequence_id']]
     order_df['to_order'] = ['']*len(igblast_makedb)
-    # print(order_df.head())
     for i in range(len(igblast_makedb)): # *** TEMP loop through all of igblast_makedb
         # select dictionary index
         locus = igblast_makedb.loc[i,'locus']
@@ -97,17 +112,19 @@ def main():
                 v_calls = curr_v_call.split(',')
                 codon_list = [var_df[var_df['v_call'] == v_call]['var'].item() for v_call in v_calls]
                 codon_set = set(codon_list)
+
                 if len(codon_set) == 1:
                     var = codon_list[0]
                 else:
+                    # Default behavior to produce an error when two v_call are given with different codons
                     print('ERROR @', i)
                     order_df.loc[i, 'to_order'] = 'ERROR'
                     continue
             else:
-                var = var_df[var_df['v_call']==curr_v_call]['var'].values
+                var = var_df[var_df['v_call']==curr_v_call]['var'].values[0]
         else:
             var = ''
-
+        
         # Construct insert
         order_df.loc[i, 'to_order'] = str(consts_df['start'][dict_selector] + \
                                         var + \
@@ -121,19 +138,11 @@ def main():
     for i in range(len(order_df)):
         prev_seq = order_df.loc[i, 'to_order']
         order_df.loc[i, 'to_order'] = restriction_replacement(order_df.loc[i, 'to_order'], dict_selector)
-        
-        # if replaced, test for restriction site replacement again
-        attempt_counter = 0
         if prev_seq != order_df.loc[i, 'to_order']:
-            i -= 1
-            attempt_counter += 1
-            
-            if attempt_counter == 6:
-                print('Restriction site error at ' + i + '. Unable to replace duplicate site.')
-
+            print(f'Restriction site(s) within internal region detected and replaced at row index {i}.')
+    
     # save
-    order_df.to_csv('./to_order.csv')
-
+    order_df.to_csv('./'+args.data.split('/')[-1].split('.')[0]+'.to_order.csv')
 
 if __name__ == '__main__':
     main()
